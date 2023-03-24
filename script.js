ecf4d6719f808fd9bcbf3982eaec44799730af73
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-03-24 18:07:02 -0400

    1.01 7am temperature correctly pulled from weather.gov API and printer in browser

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 function getWeather(zipCode) {
-    const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${zipCode}&key=AIzaSyBDTl65SgR2nqysCNCzlE5fiGikIRjDc98`;
+    mapsKey = "AIzaSyBDTl65SgR2nqysCNCzlE5fiGikIRjDc98";
+    //https://console.cloud.google.com/apis --> get key for API here
+    const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${zipCode}&key=${mapsKey}`;
+    console.log(geocodeUrl);
     fetch(geocodeUrl)
       .then(response => response.json())
       .then(data => {
         const location = data.results[0].geometry.location;
         console.log(location);
+        //https://www.weather.gov/documentation/services-web-api  --> documentation for this API
         const weatherUrl = `https://api.weather.gov/points/${location.lat},${location.lng}`;
+        console.log(weatherUrl);
         fetch(weatherUrl, {
           headers: {
             'User-Agent': 'calvaryeagles.org (langdon@calvaryeagles.org)'
@@ -13,15 +18,23 @@ function getWeather(zipCode) {
         })
           .then(response => response.json())
           .then(data => {
-            const tomorrow7amForecast = data.properties.periods.find(period => {
-              const date = new Date(period.startTime);
-              return date.getDate() === (new Date().getDate() + 1) && date.getHours() === 7;
-            });
-            if (tomorrow7amForecast) {
-              document.getElementById('temp-tomorrow').textContent = `Temperature at 7am tomorrow: ${tomorrow7amForecast.temperature} °F`;
-            } else {
-              document.getElementById('temp-tomorrow').textContent = 'No forecast available for 7am tomorrow';
-            }
+            const forecastHourlyUrl = data.properties.forecastHourly;
+            fetch(forecastHourlyUrl, {
+              headers: {
+                'User-Agent': 'calvaryeagles.org (langdon@calvaryeagles.org)'
+              }
+            })
+            .then(response => response.json())
+            .then(data => {
+              const tomorrow7amForecast = data.properties.periods.find(period => {
+                return period.startTime.includes('T07:00');
+              });
+              if (tomorrow7amForecast) {
+                document.getElementById('temp-tomorrow').textContent = `Temperature at 7am tomorrow: ${tomorrow7amForecast.temperature} °F`;
+              } else {
+                document.getElementById('temp-tomorrow').textContent = 'No forecast available for 7am tomorrow';
+              }
+            })
           })
           .catch(error => {
             console.error('Error fetching weather data:', error);
