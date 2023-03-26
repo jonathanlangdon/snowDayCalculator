7b2a6e687f6bb8aa3d358cb067398298c004181f
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-03-26 15:31:19 -0400

    1.05 starting to add location grabbing button

--- script.js
+++ script.js
@@ -64,3 +64,31 @@ function getWeather(zipCode) {
     const zipCode = document.querySelector('#zip-code').value;
     getWeather(zipCode);
   });
+
+  // Get the button element
+const locationBtn = document.querySelector("button[type='button']");
+
+// Add a click event listener to the button
+locationBtn.addEventListener("click", () => {
+  // Check if the geolocation API is supported by the browser
+  if ("geolocation" in navigator) {
+    // Request the user's location
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        // Get the latitude and longitude coordinates
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+        
+        // Use the coordinates to get the user's location
+        console.log(lat + " latitude and " + lon + " longitude");
+      },
+      error => {
+        // Handle errors if location can't be retrieved
+        console.log(error.message);
+      }
+    );
+  } else {
+    // Handle the case where geolocation isn't supported by the browser
+    console.log("Geolocation is not supported by your browser.");
+  }
+});
