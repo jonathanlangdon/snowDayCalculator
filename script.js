ebc4dd050f809e1fda5ebf43d05d11d5ce45a3d6
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-09-23 22:20:18 -0400

    major updates, bootstrap, getting API fully working

--- script.js
+++ script.js
@@ -1,100 +1,132 @@
-let latitude = 0;
-let longitude = 0;
+function errorGettingWeather() {
+  document
+    .getElementById('error-container')
+    .insertAdjacentText('beforeend', '|| Error fetching weather data ||')
+}
 
-function getCoordinates(zipCode) {
-  const mapsKey = "AIzaSyBDTl65SgR2nqysCNCzlE5fiGikIRjDc98";
-  //https://console.cloud.google.com/apis --> get key for API here
-  const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${zipCode}&key=${mapsKey}`;
-  console.log(geocodeUrl);
-  fetch(geocodeUrl)
+function getWeatherUrl() {
+  const latitude = document.querySelector('#latitude').value
+  const longitude = document.querySelector('#longitude').value
+  return `https://api.weather.gov/points/${latitude},${longitude}`
+}
+
+function fetchWeatherData(url) {
+  return fetch(url).then(response => response.json())
+}
+
+function getForecastForTime(data, time) {
+  return data.properties.periods.find(period => period.startTime.includes(time))
+}
+
+function updateElementValue(elementId, value) {
+  document.getElementById(elementId).value = value
+}
+
+function displayError(message) {
+  document
+    .getElementById('error-container')
+    .insertAdjacentText('beforeend', message)
+}
+
+// get 7am Forecast for Temperature
+function handleTemperatureForecast(data) {
+  const tomorrow7amForecast = getForecastForTime(data, 'T07:00')
+
+  if (tomorrow7amForecast) {
+    updateElementValue('temp', tomorrow7amForecast.temperature)
+  } else {
+    updateElementValue('temp', '')
+    displayError('|| No forecast available for 7am tomorrow ||')
+  }
+}
+
+// get 5am Forecast for Precipitation
+function handlePrecipitationForecast(data) {
+  const tomorrow5amForecast = getForecastForTime(data, 'T05:00')
+
+  if (tomorrow5amForecast) {
+    updateElementValue(
+      'precip',
+      tomorrow5amForecast.probabilityOfPrecipitation.value
+    )
+  } else {
+    updateElementValue('precip', '')
+    displayError('|| No forecast available for 5am tomorrow ||')
+  }
+}
+
+function handleForecastHourly(url) {
+  fetch(url)
     .then(response => response.json())
     .then(data => {
-      const location = data.results[0].geometry.location;
-      console.log(location);
-      latitude = location.lat;
-      longitude = location.lng;
-      console.log(latitude);
-      console.log(longitude);
+      handleTemperatureForecast(data)
+      handlePrecipitationForecast(data)
+    })
+}
+
+function handleSnow(data, day, container) {
+  const forecastOfDay = data.properties.periods[day]
+
+  if (forecastOfDay) {
+    const forecastDescription = forecastOfDay.detailedForecast
+    const regex = /\b(\d+(\.\d+)?)\s*(inch|inches)\b/
+    const match = forecastDescription.match(regex)
+    document.querySelector(container).value = match ? parseFloat(match[1]) : 0
+  } else {
+    errorGettingWeather()
+  }
+}
+
+function getWeather() {
+  const weatherUrl = getWeatherUrl()
+  fetchWeatherData(weatherUrl)
+    .then(initialData => {
+      const forecastHourlyUrl = initialData.properties.forecastHourly
+      const forecastUrl = initialData.properties.forecast
+      return fetchWeatherData(forecastUrl)
+        .then(forecastData => {
+          handleSnow(forecastData, 0, '#snow-today')
+          handleSnow(forecastData, 1, '#snow-tomorrow')
+        })
+        .then(() => {
+          return forecastHourlyUrl
+        })
+    })
+    .then(forecastHourlyUrl => {
+      return handleForecastHourly(forecastHourlyUrl)
     })
     .catch(error => {
-      console.error("Error fetching location data:", error);
-      document.getElementById("temp-tomorrow").textContent = "Error fetching location data";
-    });
-};
-    
-
-function getWeather(funLatitude,funLongitude) {
-  //https://www.weather.gov/documentation/services-web-api  --> documentation for this API
-  const weatherUrl = `https://api.weather.gov/points/${funLatitude},${funLongitude}`;
-  fetch(weatherUrl, {
-    headers: {
-      "User-Agent": "calvaryeagles.org (langdon@calvaryeagles.org)"
-    }
-  })
-  .then(response => response.json())
-  .then(data => {
-    const forecastHourlyUrl = data.properties.forecastHourly;
-    console.log(forecastHourlyUrl);
-    fetch(forecastHourlyUrl, {
-      headers: {
-        "User-Agent": "calvaryeagles.org (langdon@calvaryeagles.org)"
-      }
+      console.error('Error fetching weather data:', error)
+      errorGettingWeather()
     })
-    .then(response => response.json())
-    .then(data => {
-      const tomorrow7amForecast = data.properties.periods.find(period => {
-        return period.startTime.includes("T07:00");
-      });
-      console.log(tomorrow7amForecast);
-      if (tomorrow7amForecast) {
-        document.getElementById("temp-tomorrow").textContent = `Temperature at 7am tomorrow: ${tomorrow7amForecast.temperature} °F`;
-      } else {
-        document.getElementById("temp-tomorrow").textContent = "No forecast available for 7am tomorrow";
-      }
-      const tomorrow5amForecast = data.properties.periods.find(period => {
-        return period.startTime.includes("T05:00");
-      });
-      console.log(tomorrow5amForecast);
-      if (tomorrow5amForecast) {
-        document.getElementById("precip-chance").textContent = `Precipitation at 5am: ${tomorrow5amForecast.probabilityOfPrecipitation.value} %`;
-      } else {
-        document.getElementById("precip-chance").textContent = "No forecast available for 5am tomorrow";
-      }
-    })
-  })
-  .catch(error => {
-    console.error("Error fetching weather data:", error);
-    document.getElementById("temp-tomorrow").textContent = "Error fetching weather data";
-  });
-
 }
-  
-const zipForm = document.querySelector("#zip-form");
-zipForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const zipCode = document.querySelector("#zip-code").value;
-  getCoordinates(zipCode);
-  getWeather(latitude,longitude);
-});
 
-  // Get the button element
-  const locationBtn = document.querySelector("#location-btn");
+document
+  .querySelector('#get-forecast-form')
+  .addEventListener('submit', function (event) {
+    document.querySelector('#error-container').innerHTML = ''
+    event.preventDefault()
+    getWeather()
+  })
 
-  locationBtn.addEventListener("click", () => {
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(
-        position => {
-          latitude = position.coords.latitude;
-          longitude = position.coords.longitude;
-          getWeather(latitude, longitude);
-        },
-        error => {
-          console.error("Error getting location:", error);
-          document.getElementById("temp-tomorrow").textContent = "Error getting location";
-        }
-      );
-    } else {
-      console.log("Geolocation is not supported by your browser.");
-    }
-  });
-  
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('calculator-form')
+  form.addEventListener('submit', async function (e) {
+    e.preventDefault()
+    const snowToday = parseFloat(document.getElementById('snow-today').value)
+    const snowTomorrow = parseFloat(
+      document.getElementById('snow-tomorrow').value
+    )
+    const precip = parseFloat(document.getElementById('precip').value)
+    const temp = parseFloat(document.getElementById('temp').value)
+    const response = await fetch('http://your-server-address:5000/calculate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ snowToday, snowTomorrow, precip, temp })
+    })
+    const data = await response.json()
+    alert('Chance of Snow Day: ' + data.result)
+  })
+})
