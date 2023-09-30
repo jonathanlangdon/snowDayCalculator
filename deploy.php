b5e14137e0a6242fdd38f819c272e9b665554b6f
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-09-29 23:49:10 -0400

    setting up webhook

---
+++ deploy.php
@@ -0,0 +1,19 @@
+<?php
+$secret = getenv('GITHUB_SECRET');
+$githubSignature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+
+if (!isset($githubSignature)) {
+    die('No signature provided');
+}
+
+list($algo, $signature) = explode('=', $githubSignature, 2);
+
+$payload = file_get_contents('php://input');
+$payloadHash = hash_hmac($algo, $payload, $secret);
+
+if ($payloadHash !== $signature) {
+    die('Invalid signature');
+}
+
+echo shell_exec("cd ~/public_html && git pull 2>&1");
+?>
