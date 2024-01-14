b537bf7715ebbfb841bab8a6a588ef355b956558
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-01-14 12:57:39 -0500

    post request fixing

--- script.js
+++ script.js
@@ -186,7 +186,7 @@ async function getAnalyzeForecast(e) {
   } finally {
     document.getElementById('loading-message').style.display = 'none';
   }
-  // ALERT = await handleAlert(alertUrl);
+  ALERT = await handleAlert(alertUrl);
   const apiData = JSON.stringify({
     snowtoday: SNOWTODAY,
     snowtomorrow: SNOWTOMORROW,
