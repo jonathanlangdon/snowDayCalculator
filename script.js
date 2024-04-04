5d85bd2743595f54aca77c4139697a6149b5929b
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-04-04 11:48:22 -0400

    moving temp function to api file

--- script.js
+++ script.js
@@ -1,32 +1,11 @@
-import { getWeatherUrl } from './scripts/api.js';
-import { displayError, errorGettingWeather } from './scripts/utility.js?v=1.02';
+import { getWeatherUrl, getFeelLikeTemp } from './scripts/api.js';
+import { displayError, errorGettingWeather } from './scripts/utility.js?v=1.03';
 
 let SNOWTODAY = 0;
 let SNOWTOMORROW = 0;
 let PRECIP = 0;
 let TEMP = 32;
 let ALERT = 'none';
-
-function getFeelLikeTemp(data) {
-  const tomorrow7amForecast = data.properties.periods.find(period =>
-    period.startTime.includes('T07:00')
-  );
-  if (tomorrow7amForecast) {
-    const temp7am = parseFloat(tomorrow7amForecast.temperature);
-    const windSpeedStr = tomorrow7amForecast.windSpeed;
-    const wind7am = parseFloat(windSpeedStr.split(' ')[0]);
-    // Calculating the feel-like temperature using the WCT formula from NWS
-    const feelLikeTemp = (
-      35.74 +
-      0.6215 * temp7am -
-      35.75 * wind7am ** 0.16 +
-      0.4275 * temp7am * wind7am ** 0.16
-    ).toFixed(0);
-    return feelLikeTemp;
-  } else {
-    displayError('|| No forecast available for 7am tomorrow ||');
-  }
-}
 
 // get 5am Forecast for Precipitation
 function handlePrecipitationForecast(data) {
--- scripts/api.js
+++ scripts/api.js
@@ -9,3 +9,24 @@ export function getWeatherUrl() {
     `https://api.weather.gov/alerts/active?point=${latitude},${longitude}`
   ];
 }
+
+export function getFeelLikeTemp(data) {
+  const tomorrow7amForecast = data.properties.periods.find(period =>
+    period.startTime.includes('T07:00')
+  );
+  if (tomorrow7amForecast) {
+    const temp7am = parseFloat(tomorrow7amForecast.temperature);
+    const windSpeedStr = tomorrow7amForecast.windSpeed;
+    const wind7am = parseFloat(windSpeedStr.split(' ')[0]);
+    // Calculating the feel-like temperature using the WCT formula from NWS
+    const feelLikeTemp = (
+      35.74 +
+      0.6215 * temp7am -
+      35.75 * wind7am ** 0.16 +
+      0.4275 * temp7am * wind7am ** 0.16
+    ).toFixed(0);
+    return feelLikeTemp;
+  } else {
+    displayError('|| No forecast available for 7am tomorrow ||');
+  }
+}
